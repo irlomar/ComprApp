5ad08c1300855ed3235efbdc9139f19e8a92e45b
Countero <javicg23@yahoo.com>
2019-05-27 12:21:53 +0200

    Nuevo producto movido a controlador

--- app/src/main/java/mycompra/app/controlador/NuevoProducto.java
+++ app/src/main/java/mycompra/app/controlador/NuevoProducto.java
@@ -3,11 +3,15 @@ package mycompra.app.controlador;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import mycompra.app.R;
+import mycompra.app.controlador.Productos;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +28,30 @@ public class NuevoProducto extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nuevo_producto, container, false);
+        View vista = inflater.inflate(R.layout.fragment_nuevo_producto, container, false);
+
+        Button btnAceptar = (Button) vista.findViewById(R.id.aceptarNuevoProducto);
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.frame, new Productos());
+                ft.commit();
+            }
+
+        });
+
+        Button btnCancelar = (Button) vista.findViewById(R.id.cancelarNuevoProducto);
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.frame, new Productos());
+                ft.commit();
+            }
+
+        });
+        return vista;
     }
 
 }
